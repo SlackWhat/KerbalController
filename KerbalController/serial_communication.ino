6c101775b337d6892cbef651b9a14219db6fdcf6
SlackWhat <antoine.plasait@gmail.com>
2020-06-22 11:40:43 +0200

    Update serial_communication.ino

--- KerbalController/serial_communication.ino
+++ KerbalController/serial_communication.ino
@@ -1,3 +1,7 @@
+// ----------------------
+//        TO CHECK
+// ----------------------
+
 uint8_t rx_len;
 uint16_t * address;
 byte buffer[256]; //address for temporary storage and parsing buffer
@@ -15,7 +19,7 @@ struct HandShakePacket
 
 HandShakePacket HPacket;
 
-void Handshake(){
+void Handshake() {
   HPacket.id = 0;
   HPacket.M1 = 3;
   HPacket.M2 = 1;
@@ -30,12 +34,12 @@ void InitTxPackets() {
 
 //This shit contains stuff borrowed from EasyTransfer lib
 boolean KSPBoardReceiveData() {
-  if ((rx_len == 0)&&(Serial.available()>3)){
+  if ((rx_len == 0)&&(Serial.available()>3)) {
     while(Serial.read()!= 0xBE) {
       if (Serial.available() == 0)
         return false;  
     }
-    if (Serial.read() == 0xEF){
+    if (Serial.read() == 0xEF) {
       rx_len = Serial.read();   
       id = Serial.read(); 
       rx_array_inx = 1;
@@ -53,27 +57,27 @@ boolean KSPBoardReceiveData() {
     }
 
     //make sure the binary structs on both Arduinos are the same size.
-    if(rx_len != structSize){
+    if (rx_len != structSize) {
       rx_len = 0;
       return false;
     }   
   }
 
-  if(rx_len != 0){
-    while(Serial.available() && rx_array_inx <= rx_len){
+  if (rx_len != 0) {
+    while(Serial.available() && rx_array_inx <= rx_len) {
       buffer[rx_array_inx++] = Serial.read();
     }
     buffer[0] = id;
 
-    if(rx_len == (rx_array_inx-1)){
+    if (rx_len == (rx_array_inx-1)) {
       //seem to have got whole message
       //last uint8_t is CS
       calc_CS = rx_len;
-      for (int i = 0; i<rx_len; i++){
+      for (int i = 0; i<rx_len; i++) {
         calc_CS^=buffer[i];
       } 
 
-      if(calc_CS == buffer[rx_array_inx-1]){//CS good
+      if (calc_CS == buffer[rx_array_inx-1]) {//CS good
         memcpy(address,buffer,structSize);
         rx_len = 0;
         rx_array_inx = 1;
@@ -91,16 +95,20 @@ boolean KSPBoardReceiveData() {
   return false;
 }
 
-void KSPBoardSendData(uint8_t * address, uint8_t len){
+void KSPBoardSendData(uint8_t * address, uint8_t len) {
   uint8_t CS = len;
   Serial.write(0xBE);
   Serial.write(0xEF);
   Serial.write(len);
   
-  for(int i = 0; i<len; i++){
+  for(int i = 0; i<len; i++) {
     CS^=*(address+i);
     Serial.write(*(address+i));
   }
   
   Serial.write(CS);
 }
+
+// ----------------------
+//     END   TO CHECK
+// ----------------------
