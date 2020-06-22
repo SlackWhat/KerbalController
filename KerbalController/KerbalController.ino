133f75eb1e0459a747fdf78c5ba5dc4bc6c41bee
SlackWhat <antoine.plasait@gmail.com>
2020-06-22 11:44:15 +0200

    Update KerbalController.ino

--- KerbalController/KerbalController.ino
+++ KerbalController/KerbalController.ino
@@ -1,54 +1,95 @@
+/*
+ * Kerbal Space Program Controller
+ * Arduino code
+ * 
+ * Modified by Morte-Couille (SlackWhat)
+ * Original work from hugopeeters (https://github.com/hugopeeters)
+ * And KSPSerialIO by zitronen (https://github.com/zitron-git/KSPSerialIO)
+ * 
+ */
+
 #include <SoftwareSerial.h>
-SoftwareSerial mySerial(15,14); //pin 14 connected to LCD, 15 unconnected
+SoftwareSerial mySerial(15,14);   // Pin 14 connected to LCD, 15 unconnected
 
-//analog pins
-const int pTHROTTLE = A0; //slide pot
-const int pTX = A1;       //translation x-axis
-const int pTY = A2;       //translation y-axis
-const int pTZ = A3;       //translation z-axis
-const int pRX = A4;       //rotation x-axis
-const int pRY = A5;       //rotation y-axis
-const int pRZ = A6;       //rotation z-axis
+// Analog pins
+const int pTHROTTLE = A0;   // Slide pot
 
-//digital pins
-const int pPOWER = 2;       //power switch
-const int pTB = 3;          //translation joystick button
-const int pRB = 4;          //rotation joystick button
-const int latchPin = 8;     //ST_CP - green
-const int dataPin = 11;     //DS - yellow
-const int clockPin = 12;    //SH_CP - blue
-const int pMODE = 22;       //mode switch (used for debug mode)
-const int pLCDx = 27;       //toggle switch x (used for LCD display modes)
-const int pLCDy = 24;       //toggle switch y (used for LCD display modes)
-const int pLCDz = 29;       //toggle switch z (used for LCD display modes)
-const int pSAS = 26;        //SAS switch
-const int pRCS = 31;        //RCS switch
-const int pABORT = 28;      //Abort switch (safety switch, active high)
-const int pARM = 30;        //Arm switch (safety switch, active high)
-const int pSTAGE = 32;      //Stage button
-const int pSTAGELED = 33;   //Stage button LED
-const int pLIGHTS = 34;     //Lights button
-const int pLIGHTSLED = 35;  //Lights button LED
-const int pLADDER = 36;     //Ladder button (action group 5)
-const int pLADDERLED = 37;  //Ladder button LED
-const int pSOLAR = 38;      //Solar button (action group 6)
-const int pSOLARLED = 39;   //Solar button LED
-const int pCHUTES = 40;     //Chutes button (action group 7)
-const int pCHUTESLED = 41;  //Chutes button LED
-const int pGEARS = 42;      //Gears button
-const int pGEARSLED = 43;   //Gears button LED
-const int pBRAKES = 44;     //Brakes button
-const int pBRAKESLED = 45;  //Brakes button LED
-const int pACTION1 = 46;    //Action Group 1 button
-const int pACTION1LED = 47; //Action Group 1 button LED
-const int pACTION2 = 48;    //Action Group 2 button
-const int pACTION2LED = 49; //Action Group 2 button LED
-const int pACTION3 = 50;    //Action Group 3 button
-const int pACTION3LED = 51; //Action Group 3 button LED
-const int pACTION4 = 52;    //Action Group 4 button
-const int pACTION4LED = 53; //Action Group 4 button LED
+// ----------------------
+//        TO CHECK
+// ----------------------
 
-//in-game state used to update button LEDs
+const int pTX = A1;         // Translation X-axis
+const int pTY = A2;         // Translation Y-axis
+const int pTZ = A3;         // Translation Z-axis
+
+const int pRX = A4;         // Rotation X-axis
+const int pRY = A5;         // Rotation Y-axis
+const int pRZ = A6;         // Rotation Z-axis
+
+/*
+const int pTX = A4;         // Translation X-axis
+const int pTY = A3;         // Translation Y-axis
+const int pTZ = A1;         // Translation Z-axis
+
+const int pRX = A2;         // Rotation X-axis
+const int pRY = A5;         // Rotation Y-axis
+const int pRZ = A6;         // Rotation Z-axis
+*/
+
+//toggle button states MODIFIED TO FALSE
+bool tb_on = false;
+bool rb_on = false;
+
+// Previous button state
+bool tb_prev = false;
+bool rb_prev = false;
+
+int navballMode;
+int sasMode;
+
+// ----------------------
+//     TO CHECK END
+// ----------------------
+
+// Digital pins
+const int pPOWER = 2;       // Power switch
+const int pTB = 3;          // Translation joystick button
+const int pRB = 4;          // Rotation joystick button
+const int latchPin = 8;     // ST_CP - green wire
+const int dataPin = 10;     // DS - yellow wire
+const int clockPin = 11;    // SH_CP - blue wire
+const int pMODE = 22;       // Mode switch (used for debug mode)
+const int pLCDx = 29;       // Toggle switch x (used for LCD display modes)
+const int pLCDy = 24;       // Toggle switch y (used for LCD display modes)
+const int pLCDz = 27;       // Toggle switch z (used for LCD display modes)
+const int pSAS = 26;        // SAS switch
+const int pRCS = 31;        // RCS switch
+const int pABORT = 28;      // Abort switch (safety switch, active high)
+const int pARM = 30;        // Arm switch (safety switch, active high)
+const int pSTAGE = 32;      // Stage button
+const int pSTAGELED = 33;   // Stage button LED
+const int pLIGHTS = 34;     // Lights button
+const int pLIGHTSLED = 35;  // Lights button LED
+const int pLADDER = 36;     // Ladder button (action group 5)
+const int pLADDERLED = 37;  // Ladder button LED
+const int pSOLAR = 38;      // Solar button (action group 6)
+const int pSOLARLED = 39;   // Solar button LED
+const int pCHUTES = 40;     // Chutes button (action group 7)
+const int pCHUTESLED = 41;  // Chutes button LED
+const int pGEARS = 42;      // Gears button
+const int pGEARSLED = 43;   // Gears button LED
+const int pBRAKES = 44;     // Brakes button
+const int pBRAKESLED = 45;  // Brakes button LED
+const int pACTION1 = 46;    // Action Group 1 button
+const int pACTION1LED = 47; // Action Group 1 button LED
+const int pACTION2 = 48;    // Action Group 2 button
+const int pACTION2LED = 51; // Action Group 2 button LED
+const int pACTION3 = 50;    // Action Group 3 button
+const int pACTION3LED = 49; // Action Group 3 button LED
+const int pACTION4 = 52;    // Action Group 4 button
+const int pACTION4LED = 53; // Action Group 4 button LED
+
+// In-game state - Used to update button LEDs
 bool lights_on = false;
 bool ladder_on = false;
 bool solar_on = false;
@@ -61,18 +102,10 @@ bool action2_on = false;
 bool action3_on = false;
 bool action4_on = false;
 
-//toggle button states
-bool tb_on = true;
-bool rb_on = true;
-
-//previous button state
-bool tb_prev = false;
-bool rb_prev = false;
-
-//stage LED state
+// Stage button LED state
 bool stage_led_on = false;
 
-//input value variables
+// Input value variables
 int throttle_value;
 int tx_value;
 int ty_value;
@@ -81,191 +114,63 @@ int rx_value;
 int ry_value;
 int rz_value;
 
-//variables used for fuel guages
+// Variables used for fuel gauges
 byte inputBytes[7];
 int vSF, vLF, vOX, vEL, vMP, SF, LF, OX, EL, MP;
 
-//debug variable
-bool debug = false;
+// Timing
+const int IDLETIMER = 20000;        // If no message received from KSP for more than 20s, go idle (default 2000)
+const int CONTROLREFRESH = 10;      // Send control packet every 10 ms (default 25)
+const int stageLedBlinkTime = 500;  // Blink staging LED when armed every 500 ms
 
-//timing
-const int IDLETIMER = 20000;        //if no message received from KSP for more than 20s, go idle (default 2000)
-const int CONTROLREFRESH = 10;      //send control packet every 10 ms (default 25)
-const int stageLedBlinkTime = 500;  //blink staging LED when armed every 500 ms
-
-//variables used in timing
+// Variables used in timing
 unsigned long deadtime, deadtimeOld, controlTime, controlTimeOld, stageLedTime, stageLedTimeOld;
 unsigned long now;
 
-//variables used in serial communication
+// Variables used in serial communication
 boolean Connected = false;
 byte id;
 
+// Other variables
+long randomNumber;    // Random numer for welcome message
+
+
 void setup() {
   
-  Serial.begin(38400);  //KSPSerialIO connection
-  mySerial.begin(9600); //LCD connection
-  delay(500);           //wait for LCD boot
-  
-  //write to LCD
-  clearLCD();
-  writeLCD("KerbalController");
-  jumpToLineTwo();
-  writeLCD("booting...");
-  delay(100);
+  randomSeed(analogRead(0));    // Init random()
+  Serial.begin(38400);          // KSPSerialIO connection
+  mySerial.begin(9600);         // LCD connection
 
-  //Initialize
-  controlsInit();   //set all pin modes
-  testLEDS(50);     //blink every LED once to test (with a delay of 10 ms)
-  InitTxPackets();  //initialize the serial communication
+  // Initializing controller
+  controlsInit();               // Set all pin modes
+  InitTxPackets();              // Initialize the serial communication
+  if (!digitalRead(pPOWER))
+    testLEDS(50);               // Blink every LED once to test (with a delay of 10 ms)
+  
+  //delay(500);                   // Wait for LCD boot
+  //setLCDlight(157);             // Making sure LCD backlight is on
+  //delay(500);
+
+  // Initializing controller
+  
+  
+  clearLCD();
+  if (!digitalRead(pPOWER))
+    welcomeMessage();           // LCD Welcome message
+
 }
 
 void loop() {
   
-  //check mode
-  if(!digitalRead(pMODE)){debug = true;} else {debug = false;}
+  if (digitalRead(pPOWER))
+    shutControllerOff ();     // Controller is OFF
 
-  if(debug){
-    //Debug mode does not communicate with KSPSerialIO, but simply displays the button states on the LCD.
-    //Select analog input using LCDx/y/z. Xyz = Throttle. xYz = Translation. xyZ = Rotation.
-
-    //previous state tracking only used in debug
-    bool stage_prev = false;
-    bool chutes_prev = false;
-    bool action1_prev = false;
-    bool action2_prev = false;
-    bool action3_prev = false;
-    bool action4_prev = false;
-    bool lights_prev = false;
-    bool ladder_prev = false;
-    bool solar_prev = false;
-    bool gears_prev = false;
-    bool brakes_prev = false;
-
-    //clear the LCD 
-    clearLCD();
-    
-    //toggle switches
-    if(!digitalRead(pSAS)){writeLCD("S");} else {writeLCD("s");}
-    if(!digitalRead(pRCS)){writeLCD("R");} else {writeLCD("r");}
-    if(digitalRead(pABORT)){writeLCD("A");} else {writeLCD("a");} //note abort switch is active high
-    if(digitalRead(pARM)){writeLCD("A");} else {writeLCD("a");}   //note arm switch is active high
-   
-    //in debug mode, handle all buttons as toggle buttons
-
-    if(!digitalRead(pSTAGE) && !stage_prev){stage_on = !stage_on; stage_prev = true;}
-    if(digitalRead(pSTAGE) && stage_prev){stage_prev = false;}
-    if(stage_on){writeLCD("S");} else {writeLCD("s");}
-    digitalWrite(pSTAGELED, stage_on);
-
-    if(!digitalRead(pLIGHTS) && !lights_prev){lights_on = !lights_on; lights_prev = true;}
-    if(digitalRead(pLIGHTS) && lights_prev){lights_prev = false;}
-    if(lights_on){writeLCD("L");} else {writeLCD("l");}
-    digitalWrite(pLIGHTSLED, lights_on);
-    
-    if(!digitalRead(pLADDER) && !ladder_prev){ladder_on = !ladder_on; ladder_prev = true;}
-    if(digitalRead(pLADDER) && ladder_prev){ladder_prev = false;}
-    if(ladder_on){writeLCD("L");} else {writeLCD("l");}
-    digitalWrite(pLADDERLED, ladder_on);
-    
-    if(!digitalRead(pSOLAR) && !solar_prev){solar_on = !solar_on; solar_prev = true;}
-    if(digitalRead(pSOLAR) && solar_prev){solar_prev = false;}
-    if(solar_on){writeLCD("S");} else {writeLCD("s");}
-    digitalWrite(pSOLARLED, solar_on);
-  
-    if(!digitalRead(pCHUTES) && !chutes_prev){chutes_on = !chutes_on; chutes_prev = true;}
-    if(digitalRead(pCHUTES) && chutes_prev){chutes_prev = false;}
-    if(chutes_on){writeLCD("C");} else {writeLCD("c");}
-    digitalWrite(pCHUTESLED, chutes_on);
-    
-    if(!digitalRead(pGEARS) && !gears_prev){gears_on = !gears_on; gears_prev = true;}
-    if(digitalRead(pGEARS) && gears_prev){gears_prev = false;}
-    if(gears_on){writeLCD("G");} else {writeLCD("g");}
-    digitalWrite(pGEARSLED, gears_on);
-  
-    if(!digitalRead(pBRAKES) && !brakes_prev){brakes_on = !brakes_on; brakes_prev = true;}
-    if(digitalRead(pBRAKES) && brakes_prev){brakes_prev = false;}
-    if(brakes_on){writeLCD("B");} else {writeLCD("b");}
-    digitalWrite(pBRAKESLED, brakes_on);
-
-    if(!digitalRead(pACTION1) && !action1_prev){action1_on = !action1_on; action1_prev = true;}
-    if(digitalRead(pACTION1) && action1_prev){action1_prev = false;}
-    if(action1_on){writeLCD("A");} else {writeLCD("a");}
-    digitalWrite(pACTION1LED, action1_on);
-    
-    if(!digitalRead(pACTION2) && !action2_prev){action2_on = !action2_on; action2_prev = true;}
-    if(digitalRead(pACTION2) && action2_prev){action2_prev = false;}
-    if(action2_on){writeLCD("A");} else {writeLCD("a");}
-    digitalWrite(pACTION2LED, action2_on);
-    
-    if(!digitalRead(pACTION3) && !action3_prev){action3_on = !action3_on; action3_prev = true;}
-    if(digitalRead(pACTION3) && action3_prev){action3_prev = false;}
-    if(action3_on){writeLCD("A");} else {writeLCD("a");}
-    digitalWrite(pACTION3LED, action3_on);
-    
-    if(!digitalRead(pACTION4) && !action4_prev){action4_on = !action4_on; action4_prev = true;}
-    if(digitalRead(pACTION4) && action4_prev){action4_prev = false;}
-    if(action4_on){writeLCD("A");} else {writeLCD("a");}
-    digitalWrite(pACTION4LED, action4_on);
-    
-    if(!digitalRead(pTB) && !tb_prev){tb_on = !tb_on; tb_prev = true;}
-    if(digitalRead(pTB) && tb_prev){tb_prev = false;}
-    if(tb_on){writeLCD("T");} else {writeLCD("t");}
-  
-    if(!digitalRead(pRB) && !rb_prev){rb_on = !rb_on; rb_prev = true;}
-    if(digitalRead(pRB) && rb_prev){rb_prev = false;}
-    if(rb_on){writeLCD("R");} else {writeLCD("r");}
-  
-    //analog inputs
-    if(!digitalRead(pLCDx) && digitalRead(pLCDy) && digitalRead(pLCDz)){
-      throttle_value = analogRead(pTHROTTLE);
-      char throttle_char[5];
-      itoa(throttle_value, throttle_char, 10);
-      writeLCD(throttle_char);
-      writeLCD(" ");
-    }
-    if(digitalRead(pLCDx) && !digitalRead(pLCDy) && digitalRead(pLCDz)){
-      tx_value = analogRead(pTX);
-      char tx_char[5];
-      itoa(tx_value, tx_char, 10);
-      writeLCD(tx_char);
-      writeLCD(" ");
-      ty_value = analogRead(pTY);
-      char ty_char[5];
-      itoa(ty_value, ty_char, 10);
-      writeLCD(ty_char);
-      writeLCD(" ");
-      tz_value = analogRead(pTZ);
-      char tz_char[5];
-      itoa(tz_value, tz_char, 10);
-      writeLCD(tz_char);
-      writeLCD(" ");
-    }
-     
-    if(digitalRead(pLCDx) && digitalRead(pLCDy) && !digitalRead(pLCDz)){ 
-      rx_value = analogRead(pRX);
-      char rx_char[5];
-      itoa(rx_value, rx_char, 10);
-      writeLCD(rx_char);
-      writeLCD(" ");    
-      ry_value = analogRead(pRY);
-      char ry_char[5];
-      itoa(ry_value, ry_char, 10);
-      writeLCD(ry_char);
-      writeLCD(" ");
-      rz_value = analogRead(pRZ);
-      char rz_char[5];
-      itoa(rz_value, rz_char, 10);
-      writeLCD(rz_char);
-    }
-    //end of debug mode
-  }
+  if(!digitalRead(pMODE))
+    debug_mode();             // Debug mode
   else {
-    
-    //KSP mode
-
-    //send and receive data
-    get_vessel_data();
-    send_control_packet();
+    // KSP play mode
+    get_vessel_data();        // Receive KSP data
+    send_control_packet();    // Send data to KSP
   }
+
 }
